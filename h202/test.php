5855f33bac48198e8c30ea0572101263d96b9556
dneff68 <dneff68@gmail.com>
2020-08-01 17:59:34 -0700

    Updates

--- h202/test.php
+++ h202/test.php
@@ -1,15 +1,22 @@
-<?
+<?php
 session_start();
-include_once '/var/www/html/CHT/h202/GlobalConfig.php';
-include_once '/var/www/html/CHT/h202/h202Functions.php';
-include_once '/var/www/html/CHT/lib/chtFunctions.php';
-include_once 'chtFunctions.php';
+include_once 'GlobalConfig.php';
+include_once 'h202Functions.php';
+include_once '../lib/chtFunctions.php';
 include_once 'db_mysql.php';
 
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set('display_errors', 'On');
 
-phpinfo();
+echo $SERVERNAME . " " . $DOCUMENT_ROOT;
 
+if (david())
+{
+	echo "Hello David";	
+}
+else
+{
+	echo "Good day sir";
+}
 die;
 ?>
